d78501a5839ac08724b2de491405a8f680f019b6
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-07-05 15:02:04 +0900

    state count

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () =>{
+    this.setState({count: this.state.count + 1});
+  }
+
+  handleDecrement = () =>{
+    const counter = this.state.count - 1;
+    this.setState({count: counter < 0 ? 0 : counter});
+  }
+
   render(){
     return (
     <li className='habit'>
     <span className="habit-name">Reading</span>
-    <span className="habit-count">2</span>
-    <button class="habit-button habit-plus">
+    <span className="habit-count">{this.state.count}</span>
+    <button class="habit-button habit-plus" onClick={this.handleIncrement}>
       <i className="fas fa-plus-circle"></i>
     </button>
-    <button class="habit-button habit-minus">
+    <button class="habit-button habit-minus"  onClick={this.handleDecrement}>
       <i className="fas fa-minus-circle"></i>
     </button>
     <button class="habit-button habit-trash">
